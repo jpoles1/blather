44ca126798ec8480ca0c3744e71a0c136923124c
Jordan Poles <jpoles1@gmail.com>
2016-02-06 10:44:37 -0600

    Added new commands

--- ardlights.py
+++ ardlights.py
@@ -4,7 +4,7 @@ def main(argv):
 	thread.start_new_thread(os.system,("espeak 'Setting lights to "+color+"'",))
 	if(color in ["off", "toggle"]):
 		color="on"
-	irsig={"on": "0xFF02FD", "red": "0xFF1AE5", "blue": "0xFF9A65","green": "0xFFA25D", "white": "0xFF22DD"}
+	irsig={"on": "0xFF02FD", "red": "0xFF1AE5", "blue": "0xFF9A65","green": "0xFFA25D", "white": "0xFF22DD", "bright": "0xFF3AC5", "dim": "0xFFBA45", "strobe": "0xFFD02F", "fade": "0xFFE01F"}
 	ser = serial.Serial('/dev/ttyUSB0', 9600)
 	print(ser.name)
 	ser.write(irsig[color])
