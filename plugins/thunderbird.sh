71e0524499ba3dcfa072748718f43bbb3954ef2e
Jordan Poles <jpoles1@gmail.com>
2016-02-23 16:05:14 -0600

    cleared directory.

--- plugins/thunderbird.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-espeak "Yes sir";
-
-# Use screen to separate a program from the cancel commands
-# It will not be shut down when the voice recognition system is closed.
-screen -d -m thunderbird;
