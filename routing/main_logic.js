2650493b3c8eaee924a57e94526c813a10b313c0
Jordan Poles <jpoles1@gmail.com>
2016-02-23 18:21:13 -0600

    Ready for trials.

--- routing/main_logic.js
+++ routing/main_logic.js
@@ -2,4 +2,8 @@ module.exports = function(app){
   app.get("/", function(req, res){
     res.render("index.hbs", {})
   })
+  app.post("lights", function(req,res){
+    command = req.body.command;
+    res.send("Sent command: "+ command)
+  });
 }
