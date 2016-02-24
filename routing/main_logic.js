583c441725883b68779bfd235d40ce6289548116
Jordan Poles <jpoles1@gmail.com>
2016-02-24 16:44:54 -0600

    Added weather reports!

--- routing/main_logic.js
+++ routing/main_logic.js
@@ -1,21 +1,47 @@
 var PythonShell = require('python-shell');
-
+var speaking_now = 0;
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
 module.exports = function(app){
   app.get("/", function(req, res){
     res.render("index.hbs", {})
   })
+  app.get("/voice", function(req, res){
+    heard_command = req.query.command.toLowerCase().split(" ");
+    console.log(heard_command);
+    if(heard_command.contains("weather")){
+      runPyCommand("plugins/weather.py", {
+        args: []
+      });
+      res.send("Got Weather")
+    }
+  });
   app.get("/lights", function(req,res){
-    command = req.query.command.toLowerCase();	
+    command = req.query.command.toLowerCase();
     console.log(command);
     var options = {
       args: [command]
     };
-
-    PythonShell.run("ardlights.py", options, function (err, results) {
-      if (err) console.log("Lights error: ", err);
-      console.log('results: %j', results);
-    });
+    runPyCommand("plugins/ardlights.py", options);
     console.log("Sent command: "+ command)
     res.send("Sent command: "+ command)
   });
+}
+function runPyCommand(command, opts){
+  command = command.replace(/[,#!$%\^&\*;:{}=`~()]/g,"");
+  if(speaking_now!=1){
+    speaking_now = 1;
+    PythonShell.run(command, opts, function (err, results) {
+      if (err) console.log("Lights error: ", err);
+      console.log('results: %j', results);
+      speaking_now = 0;
+    });
+  }
 }
