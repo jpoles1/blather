3465410b13920f9f09eff87fd843389346281567
Jordan Poles <jpoles1@gmail.com>
2016-03-07 15:03:50 -0600

    Small fixes.

--- logic/domoLights.js
+++ logic/domoLights.js
@@ -19,13 +19,14 @@ module.exports = function(domoValidate, domoActuate){
   domoLights.setStrip = function(command, socket){
     var command_list = domoValidate.checkLEDTag(command);
     if(command_list.length>0){
+      command_list.unshift("strip")
       var options = {
         args: command_list
       };
       domoActuate.speak("Setting Lights to "+command, function(){
         socket.emit("ready")
       })
-      domoActuate.runPyCommand("plugins/ardlights.py strip", options);
+      domoActuate.runPyCommand("plugins/ardlights.py", options);
       socket.emit("msg", "Sent LED Strip command: "+ command)
     }
     else{
