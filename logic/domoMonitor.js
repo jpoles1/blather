2284ea05f1ea1e3517b517d12a1eee0583cc6b4f
Jordan Poles <jpoles1@gmail.com>
2016-03-24 22:48:03 -0500

    Last PIR indicator

--- logic/domoMonitor.js
+++ logic/domoMonitor.js
@@ -75,6 +75,7 @@ module.exports = function(app, room_status, domoSerial){
         domoSerial.DomoBehaviour.find().sort("-time").limit(10).exec(function(err, behaviourdata){
           if(typeof res != "undefined"){
             res.render("charts.hbs", {
+              last_on: room_stats["lastpir"]
               behaviourdata: behaviourdata,
               roomdata: JSON.stringify(roomdata.reverse()),
               eventdata: JSON.stringify(eventdata.reverse())
