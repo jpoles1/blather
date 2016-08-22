da210dd0456b892bc26093144bb06e8fc47f2e49
Jordan Poles <jpoles1@gmail.com>
2016-08-22 10:54:50 -0400

    Fan mic fix?

--- res/js/mic.js
+++ res/js/mic.js
@@ -115,6 +115,17 @@ $(function(){
           }
         }
       },
+      '(set) (change) van (to) *tag': function(tag) {
+        if(commandReady || !keyword_active){
+          if(domoValidate.checkFanTag(tag)){
+            socket.emit("fan", tag)
+          }
+          else{
+            $("#notify").html("Did not recognize lamp command!");
+            socket.emit("confused");
+          }
+        }
+      },
       '(enter) (activate) (start) :tag mode': function(tag){
         if(commandReady || !keyword_active){
           tag = tag.toLowerCase();
